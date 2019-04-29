fce220532f3d03be250443178f92b75dfce8d847
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-04-29 14:55:08 +0800

    first

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,46 @@
+const { hash, compare } = require('bcrypt')
+const { sign } = require('jsonwebtoken')
+const { getUserId, AuthError } = require('../utils/getUserId')
+
+const Mutation = {
+  signup: async (parent, { name, email, password }, context) => {
+    const hashedPassword = await hash(password, 10)
+    const user = await context.prisma.createUser({
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    const token = sign({ userId: user.id }, process.env.APP_SECRET)
+
+    return {
+      token,
+      user,
+    }
+  },
+  login: async (parent, { email, password }, context) => {
+    const user = await context.prisma.user({ email })
+    if (!user) {
+      throw new Error(`No user found for this email`)
+    }
+    const passwordValid = await compare(password, user.password)
+
+    if (!passwordValid) {
+      throw new Error(`Invalid Password`)
+    }
+    const token = sign({ userId: user.id }, process.env.APP_SECRET)
+
+    return {
+      token,
+      user,
+    }
+  },
+  logout: (parent, { id }, context) => {
+    //
+    return { message: 'Goodbye' }
+  },
+}
+
+module.exports = {
+  Mutation,
+}
---
+++ src/resolvers/User.js
@@ -0,0 +1,9 @@
+const User = {
+  // lists: ({ id }, args, context) => {
+  //   return context.prisma.user({ id }).lists()
+  // },
+}
+
+module.exports = {
+  User,
+}
